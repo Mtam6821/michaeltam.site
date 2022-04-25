3fe26ca9243e8007d1455873490346314f8c77c9
Mtam6821 <74935916+Mtam6821@users.noreply.github.com>
2022-04-25 12:13:04 -0700

    hello world php

---
+++ cgi-bin/php-hello-html.php
@@ -0,0 +1,22 @@
+<?php
+echo "Cache-Control: no-cache\n";
+echo "Content-type: text/html\n\n";
+echo "<html>";
+echo "<head>";
+echo "<title>Hello, PHP!</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Hello, PHP!</h1>";
+echo "<p>This page was generated with the PHP programming langauge</p>";
+
+date_default_timezone_set('America/Los_Angeles');
+$date = date('l jS \of F Y h:i:s A');
+echo "<p>Current Time: $date </p>";
+
+# IP Address is an environment variable when using CGI
+$address = getenv('REMOTE_ADDR');
+echo "<p>Your IP Address: $address</p>";
+
+echo "</body>";
+echo "</html>";
+?>
---
+++ cgi-bin/php-hello-json.php
@@ -0,0 +1,14 @@
+<?php
+echo "Cache-Control: no-cache\n";
+echo "Content-type: text/html\n\n";
+
+date_default_timezone_set('America/Los_Angeles');
+$date = date('l jS \of F Y h:i:s A');
+
+# IP Address is an environment variable when using CGI
+$address = getenv("REMOTE_ADDR");
+
+$message = array('title' => 'Hello, PHP!', 'heading' => 'Hello, PHP!', 'message' => 'This page was generated with the PHP programming language', 'time' => $date, 'IP' => $address);
+$json = json_encode($message);
+echo "$json\n";
+?>
