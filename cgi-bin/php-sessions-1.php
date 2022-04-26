01c60d594303b17da380922f7ad567585610194a
Michael <mtam6821@gmail.com>
2022-04-26 13:11:21 -0700

    fix

--- cgi-bin/php-sessions-1.php
+++ cgi-bin/php-sessions-1.php
@@ -1,7 +1,7 @@
 <?php
 
 $name = "";
-if (isset($_SESSION["name"])) {
+if (isset($POST["username"])) {
     $name = $_SESSION["name"];
 }
 else {
@@ -20,7 +20,7 @@ echo "<h1>Perl Sessions Page 1</h1>";
 echo "<p><b>Name:</b> $name";
 
 echo "<br/><br/>";
-echo "<a href=\"/cgi-bin/php-sessions-2.pl\">Session Page 2</a><br/>";
+echo "<a href=\"/cgi-bin/php-sessions-2.php\">Session Page 2</a><br/>";
 echo "<a href=\"/cgi-bin/php-cgiform.php\">PHP CGI Form</a><br />";
 echo "<form style=\"margin-top:30px\" action=\"/cgi-bin/php-destroy-session.pl\" method=\"get\">";
 echo "<button type=\"submit\">Destroy Session</button>";
